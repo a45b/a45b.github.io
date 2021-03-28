2150dc531631dd0f1342bbabf2b69abfe8c3e2d6
Kamal dev Bhuyan <kamaldev.bhuyan@gmail.com>
2021-03-28 13:23:25 +0530

    (feat): setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,70 +1,69 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import {
+  Box,
+  Button,
+  Stack,
+  Wrap,
+  WrapItem,
+  Avatar,
+  Tooltip,
+  Link,
+} from '@chakra-ui/react'
+import { Heading } from '@chakra-ui/react'
+import { FiType, FiLinkedin, FiGithub } from 'react-icons/fi'
+import React from 'react'
 
 export const Home = (): JSX.Element => (
   <div className="container">
-    <Head>
-      <title>Create Next App</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+    <Wrap>
+      <WrapItem>
+        <Avatar
+          size="2xl"
+          name="a45b"
+          margin={10}
+          src="https://avatars.githubusercontent.com/u/1850968?v=4"
+        />
+      </WrapItem>
+    </Wrap>
 
-    <main>
-      <h1 className="title">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
-      </h1>
-
-      <p className="description">
-        Get started by editing <code>pages/index.tsx</code>
-      </p>
-
-      <button
-        onClick={() => {
-          window.alert('With typescript and Jest')
-        }}
+    <Box bg="tomato" w="100%" p={4} color="white">
+      <Heading
+        as="h1"
+        size="4xl"
+        display="flex"
+        fontWeight="normal"
+        alignItems="center"
+        justifyContent="center"
+        fontFamily="'Lovers Quarrel', cursive"
       >
-        Test Button
-      </button>
+        Kamal Dev
+      </Heading>
+    </Box>
 
-      <div className="grid">
-        <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+    <Stack direction="row" spacing={4} marginTop={10}>
+      <Tooltip label="Blog" fontSize="md">
+        <Link href="https://medium.com/@kamal-dev" isExternal>
+          <Button colorScheme="teal" variant="outline">
+            <FiType />
+          </Button>
+        </Link>
+      </Tooltip>
 
-        <a href="https://nextjs.org/learn" className="card">
-          <h3>Learn &rarr;</h3>
-          <p>Learn about Next.js in an interactive course with quizzes!</p>
-        </a>
+      <Tooltip label="Github" fontSize="md">
+        <Link href="https://github.com/a45b" isExternal>
+          <Button colorScheme="teal" variant="outline">
+            <FiGithub />
+          </Button>
+        </Link>
+      </Tooltip>
 
-        <a
-          href="https://github.com/vercel/next.js/tree/master/examples"
-          className="card"
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Discover and deploy boilerplate example Next.js projects.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          className="card"
-        >
-          <h3>Deploy &rarr;</h3>
-          <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-        </a>
-      </div>
-    </main>
-
-    <footer>
-      <a
-        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Powered by{' '}
-        <Image src="/vercel.svg" alt="Vercel Logo" height={'32'} width={'64'} />
-      </a>
-    </footer>
-
+      <Tooltip label="Linkedin" fontSize="md">
+        <Link href="https://www.linkedin.com/in/k-a-m-a-l-d-e-v" isExternal>
+          <Button colorScheme="teal" variant="outline">
+            <FiLinkedin />
+          </Button>
+        </Link>
+      </Tooltip>
+    </Stack>
     <style jsx>{`
       .container {
         min-height: 100vh;
@@ -73,122 +72,6 @@ export const Home = (): JSX.Element => (
         flex-direction: column;
         justify-content: center;
         align-items: center;
-      }
-
-      main {
-        padding: 5rem 0;
-        flex: 1;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-      }
-
-      footer {
-        width: 100%;
-        height: 100px;
-        border-top: 1px solid #eaeaea;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-
-      footer img {
-        margin-left: 0.5rem;
-      }
-
-      footer a {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-
-      a {
-        color: inherit;
-        text-decoration: none;
-      }
-
-      .title a {
-        color: #0070f3;
-        text-decoration: none;
-      }
-
-      .title a:hover,
-      .title a:focus,
-      .title a:active {
-        text-decoration: underline;
-      }
-
-      .title {
-        margin: 0;
-        line-height: 1.15;
-        font-size: 4rem;
-      }
-
-      .title,
-      .description {
-        text-align: center;
-      }
-
-      .description {
-        line-height: 1.5;
-        font-size: 1.5rem;
-      }
-
-      code {
-        background: #fafafa;
-        border-radius: 5px;
-        padding: 0.75rem;
-        font-size: 1.1rem;
-        font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-          DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-      }
-
-      .grid {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        flex-wrap: wrap;
-
-        max-width: 800px;
-        margin-top: 3rem;
-      }
-
-      .card {
-        margin: 1rem;
-        flex-basis: 45%;
-        padding: 1.5rem;
-        text-align: left;
-        color: inherit;
-        text-decoration: none;
-        border: 1px solid #eaeaea;
-        border-radius: 10px;
-        transition: color 0.15s ease, border-color 0.15s ease;
-      }
-
-      .card:hover,
-      .card:focus,
-      .card:active {
-        color: #0070f3;
-        border-color: #0070f3;
-      }
-
-      .card h3 {
-        margin: 0 0 1rem 0;
-        font-size: 1.5rem;
-      }
-
-      .card p {
-        margin: 0;
-        font-size: 1.25rem;
-        line-height: 1.5;
-      }
-
-      @media (max-width: 600px) {
-        .grid {
-          width: 100%;
-          flex-direction: column;
-        }
       }
     `}</style>
 
